d46ab4350fdb953785021fa063faf4d331faa673
Elijah <elijahscherz@gmail.com>
2019-04-19 19:33:11 -0400

    Adding favicon and js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-
-    // jQuery methods go here...
-  
+$(document).ready(function() {
+  // jQuery methods go here...
+  var pattern = Trianglify({
+    width: window.innerWidth,
+    height: window.innerHeight
   });
+  var splash = document.getElementById("mainSplash");
+  var gradientString =
+    "linear-gradient(to bottom,rgba(22, 22, 22, 0.3) 0,rgba(22, 22, 22, 0.7) 85%,#161616 100%), ";
+  splash.style.backgroundImage = gradientString + "url(" + pattern.png() + ")";
+});
