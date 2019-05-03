abe8cf342a52f324710174be8b0f48dd5647cdea
Elijah <elijahscherz@gmail.com>
2019-05-03 19:38:37 -0400

    Navbar functionality

--- js/main.js
+++ js/main.js
@@ -9,3 +9,12 @@ $(document).ready(function() {
     "linear-gradient(to bottom,rgba(22, 22, 22, 0.3) 0,rgba(22, 22, 22, 0.7) 85%,#161616 100%), ";
   splash.style.backgroundImage = gradientString + "url(" + pattern.png() + ")";
 });
+
+$("nav .nav-link").on("click", function() {
+  $("nav")
+    .find(".active")
+    .removeClass("active");
+  $(this)
+    .parent()
+    .addClass("active");
+});
