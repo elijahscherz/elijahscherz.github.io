eddef3d3c8c5eb4b8977385f71d34387111b1c33
Elijah <elijahscherz@gmail.com>
2019-05-04 13:57:49 -0400

    Purple divs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // jQuery methods go here...
   var pattern = Trianglify({
     width: window.innerWidth,
@@ -8,13 +8,12 @@ $(document).ready(function() {
   var gradientString =
     "linear-gradient(to bottom,rgba(22, 22, 22, 0.3) 0,rgba(22, 22, 22, 0.7) 85%,#161616 100%), ";
   splash.style.backgroundImage = gradientString + "url(" + pattern.png() + ")";
-});
 
-$("nav .nav-link").on("click", function() {
-  $("nav")
-    .find(".active")
-    .removeClass("active");
-  $(this)
-    .parent()
-    .addClass("active");
+  $("nav .nav-link").on("click", function () {
+    $("nav")
+      .find(".active")
+      .removeClass("active");
+    $(this).addClass("active");
+  });
+
 });
