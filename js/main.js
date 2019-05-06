877841779d4365e1b4d0435b413fb64a0fd5eb50
Elijah <elijahscherz@gmail.com>
2019-05-05 23:17:01 -0400

    Fix ups

--- js/main.js
+++ js/main.js
@@ -16,4 +16,28 @@ $(document).ready(function () {
     $(this).addClass("active");
   });
 
+  typeWriter();
 });
+
+function typeWriter() {
+  var i = 0;
+  var txt1 = "Developer. Musician. Player."; /* The text */
+  var txt2 = "It's the 21st century. Everybody needs a site.";
+  var speed = 5000; /* The speed/duration of the effect in milliseconds */
+  var text1 = $("#splashText h1");
+  var text2 = $("#splashText h3");
+
+  text1.text("");
+  text2.text("");
+
+  // for (i = 0; i < txt1.length; ++i) {
+  //   text1.text(text1.text() + txt1.charAt(i));
+  //   setTimeout(typeWriter, speed);
+  // }
+
+  // for (i = 0; i < txt2.length; ++i) {
+  //   text2.text() += txt2.charAt(i);
+  //   i++;
+  //   setTimeout(typeWriter, speed);
+  // }
+}
