437a36b865dec83123d15f9cc4b1c0dec2c6826f
escherz <escherz@umich.edu>
2019-09-12 12:18:10 -0400

    Big updates to all parts of the site, especially projects

--- elijahscherz-main/src/app/projects/projects.component.ts
+++ elijahscherz-main/src/app/projects/projects.component.ts
@@ -10,29 +10,12 @@ export class ProjectsComponent implements OnInit {
 
   gists: Array<Object> = new Array<Object>();
   repos;
-
-  code = `function myFunction() {
-  document.getElementById("demo1").innerHTML = "Hello there!";
-  document.getElementById("demo2").innerHTML = "How are you?";
-  }`;
   
   constructor(private githubService: GithubService) { }
 
   ngOnInit() {
 
-    // Method to test if it was working
-    // this.githubService.getUsers().subscribe(users => {
-    //   console.log(users);
-    // })
-
-    console.log(this.githubService.getGistCode("https://gist.githubusercontent.com/elijahscherz/dae9e2c68d1877940f04d8f9e2e9f0d0/raw/7e28ff41f035a9a558356b16eb35f641a446e97f/hover_animation"));
-
-    this.githubService.getGistCode("https://gist.githubusercontent.com/elijahscherz/dae9e2c68d1877940f04d8f9e2e9f0d0/raw/7e28ff41f035a9a558356b16eb35f641a446e97f/hover_animation").subscribe(value => {
-      console.log(value);
-    });
-
     let gistsObservable = this.githubService.getGists();
-    console.log(gistsObservable);
 
     gistsObservable.subscribe((gists: any) => {
       gists.forEach((gist: any) => {
@@ -42,15 +25,11 @@ export class ProjectsComponent implements OnInit {
           let rawUrl = gist.files[key].raw_url;
           let newGistObj = gist;
           this.githubService.getGistCode(rawUrl).subscribe(value => {
-            console.log(value);
             newGistObj.content = value;
-            console.log(newGistObj.content);
             this.gists.push(newGistObj);
           });
         }
       });
-
-      console.log(this.gists);
     })
 
     this.repos = this.githubService.getRepos();
