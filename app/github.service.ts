84475d4b8f15d3a1d2b00111be235391d0b05ff7
Elijah Scherz <elijahscherz@gmail.com>
2019-09-24 17:15:55 -0400

    Pushing to live

---
+++ app/github.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  // Define GitHub API url
+  apiUrl = 'https://api.github.com/users/elijahscherz';
+
+  constructor(private http: HttpClient) {}
+
+  getGists() {
+    return this.http.get(`${this.apiUrl}/gists`);
+  }
+
+  getGistCode(rawUrl) {
+    return this.http.get(rawUrl, { responseType: "text" });
+  }
+
+  getRepos() {
+    return this.http.get(`${this.apiUrl}/repos`);
+  }
+}
