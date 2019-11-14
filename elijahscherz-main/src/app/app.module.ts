3241b4244f8560f8c02f0e6cf4f385f4eb75a5e2
Elijah Scherz <elijahscherz@gmail.com>
2019-11-14 18:26:35 -0500

    Adding a couple of needed components for new features

--- elijahscherz-main/src/app/app.module.ts
+++ elijahscherz-main/src/app/app.module.ts
@@ -1,41 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
-// import bootstrap from "bootstrap";
-import { SplashComponent } from './splash/splash.component';
-import { IntroComponent } from './intro/intro.component';
-import { ShowcasesComponent } from './showcases/showcases.component';
-import { PanelsComponent } from './panels/panels.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { ContactComponent } from './contact/contact.component';
-import { FooterComponent } from './footer/footer.component';
-import { GithubService } from './github.service';
+import { SplashComponent } from "./splash/splash.component";
+import { IntroComponent } from "./intro/intro.component";
+import { ShowcasesComponent } from "./showcases/showcases.component";
+import { PanelsComponent } from "./panels/panels.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ExperienceComponent } from "./experience/experience.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
+import { GithubService } from "./github.service";
 
 // Code Highlighting
-import { HighlightModule } from 'ngx-highlightjs';
-import javascript from 'highlight.js/lib/languages/javascript';
-import typescript from 'highlight.js/lib/languages/typescript';
-import css from 'highlight.js/lib/languages/css';
-import xml from 'highlight.js/lib/languages/xml';
-import bash from 'highlight.js/lib/languages/bash';
+import { HighlightModule } from "ngx-highlightjs";
+import javascript from "highlight.js/lib/languages/javascript";
+import typescript from "highlight.js/lib/languages/typescript";
+import css from "highlight.js/lib/languages/css";
+import xml from "highlight.js/lib/languages/xml";
+import bash from "highlight.js/lib/languages/bash";
 
 // FontAwesome
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
+// Mobile detection
+import { DeviceDetectorModule } from "ngx-device-detector";
 
 export function hljsLanguages() {
   return [
-    { name: 'typescript', func: typescript },
-    { name: 'javascript', func: javascript },
-    { name: 'css', func: css },
-    { name: 'xml', func: xml },
-    { name: 'bash', func: bash }
+    { name: "typescript", func: typescript },
+    { name: "javascript", func: javascript },
+    { name: "css", func: css },
+    { name: "xml", func: xml },
+    { name: "bash", func: bash }
   ];
 }
 
@@ -60,11 +62,10 @@ export function hljsLanguages() {
     HighlightModule.forRoot({
       languages: hljsLanguages
     }),
-    FontAwesomeModule
+    FontAwesomeModule,
+    DeviceDetectorModule.forRoot()
   ],
-  providers: [
-    GithubService
-  ],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- elijahscherz-main/src/app/projects/projects.component.ts
+++ elijahscherz-main/src/app/projects/projects.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubService } from '../github.service';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import { faJira } from '@fortawesome/free-brands-svg-icons';
-import { faBootstrap } from '@fortawesome/free-brands-svg-icons';
-import { faAngular } from '@fortawesome/free-brands-svg-icons';
-import { faPython } from '@fortawesome/free-brands-svg-icons';
-import { faNodeJs } from '@fortawesome/free-brands-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { Component, OnInit } from "@angular/core";
+import { GithubService } from "../github.service";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import { faJira } from "@fortawesome/free-brands-svg-icons";
+import { faBootstrap } from "@fortawesome/free-brands-svg-icons";
+import { faAngular } from "@fortawesome/free-brands-svg-icons";
+import { faPython } from "@fortawesome/free-brands-svg-icons";
+import { faNodeJs } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faRaspberryPi } from "@fortawesome/free-brands-svg-icons";
+import { DeviceDetectorService } from "ngx-device-detector";
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  selector: "app-projects",
+  templateUrl: "./projects.component.html",
+  styleUrls: ["./projects.component.css"]
 })
 export class ProjectsComponent implements OnInit {
-
   faCircle = faCircle;
   faJira = faJira;
   faBootstrap = faBootstrap;
@@ -22,21 +23,26 @@ export class ProjectsComponent implements OnInit {
   faPython = faPython;
   faNodeJs = faNodeJs;
   faGithub = faGithub;
+  faRaspberryPi = faRaspberryPi;
 
   gists: Array<Object> = new Array<Object>();
   repos;
-  
-  constructor(private githubService: GithubService) { }
+  isMobile: Boolean = false;
+
+  constructor(
+    private githubService: GithubService,
+    private deviceService: DeviceDetectorService
+  ) {}
 
   ngOnInit() {
+    this.isMobile = this.deviceService.isMobile();
 
     let gistsObservable = this.githubService.getGists();
 
     gistsObservable.subscribe((gists: any) => {
       gists.forEach((gist: any) => {
-        
         // Loop through the files to get the rawUrl and the code
-        for(let key of Object.keys(gist.files)) {
+        for (let key of Object.keys(gist.files)) {
           let rawUrl = gist.files[key].raw_url;
           let newGistObj = gist;
           this.githubService.getGistCode(rawUrl).subscribe(value => {
@@ -45,9 +51,8 @@ export class ProjectsComponent implements OnInit {
           });
         }
       });
-    })
+    });
 
     this.repos = this.githubService.getRepos();
   }
-
 }
