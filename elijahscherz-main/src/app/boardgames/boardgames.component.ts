2d3a98a376a9fe6dc942326591854b36ac4f144e
escherz <escherz@umich.edu>
2020-12-06 01:25:07 -0500

    Facelift the boardgames page, add sorting

--- elijahscherz-main/src/app/boardgames/boardgames.component.ts
+++ elijahscherz-main/src/app/boardgames/boardgames.component.ts
@@ -9,14 +9,40 @@ import GamesJson from '../../assets/data/games.json';
 export class BoardgamesComponent implements OnInit {
 
   games: Array<Object> = GamesJson;
+  isMobile: boolean = false;
 
   constructor() { }
 
+  onClickAlpha() {
+    this.games.sort((a, b) => {
+      return a.name.text.localeCompare(b.name.text);
+    });
+  }
+  onClickGeek() {
+    this.games.sort((a, b) => {
+      return a.stats.rating.average.value - b.stats.rating.average.value;
+    }).reverse();
+  }
+  onClickRating() {
+    // this.games.sort((a, b) => {
+    //   return a.numplays - b.numplays;
+    // }).reverse();
+  }
+  onClickPlays() {
+    this.games.sort((a, b) => {
+      return a.numplays - b.numplays;
+    }).reverse();
+  }
+
   ngOnInit() {
 
-    // Randomize the game page display
-    this.games.sort(() => Math.random() - 0.5);
+    // Fairly simple mobile detection method
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      this.isMobile = true;
+    }
 
+    // Randomize the game page display to start
+    this.games.sort(() => Math.random() - 0.5);
   }
 
 }
